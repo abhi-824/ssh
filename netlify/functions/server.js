1a2f5f9ddef0ef12448fc8cc209cbdc52348fe6d
Abhinandan Sharma <the.narcissist.coder@MacBook-Pro-od-Abhinandan.local>
2025-10-19 22:41:04 +0530

    netlfiy hosting

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,12 +1,4 @@
-import { createRequestHandler } from "@react-router/serve";
+import { createRequestHandler } from "@react-router/netlify";
 import * as build from "../../build/server/index.js";
 
-export default async function handler(req, res) {
-  try {
-    const requestHandler = createRequestHandler({ build });
-    return requestHandler(req, res);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Internal Server Error");
-  }
-}
+export default createRequestHandler({ build });
