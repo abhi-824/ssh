87e24ad83da71f6e64adfb3db3b610d8ab671614
Abhinandan Sharma <the.narcissist.coder@MacBook-Pro-od-Abhinandan.local>
2025-10-19 22:52:31 +0530

    commit

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,4 @@
-import { createRequestHandler } from "@react-router/netlify";
+import { createRequestHandler } from "@react-router/serve";
 import * as build from "../../build/server/index.js";
 
 export default createRequestHandler({ build });
